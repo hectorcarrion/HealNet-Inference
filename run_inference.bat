a4be50e98c1548ef7a6d1d6b49e4140c8bc8d849
Hector Carion <hcarrion@ucsc.edu>
2022-11-09 11:39:55 -0800

    autocommit

---
+++ run_inference.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\labuser\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe" "C:\Users\labuser\Desktop\HealNet-Inference\healnet_inference.py"
+pause
