3c303654465264d5232da03dd61f2f2783dcedf8
Hector Carrion <43754045+hectorcarrion@users.noreply.github.com>
2022-12-14 15:52:33 -0400

    Update healnet_inference.py
    Fix for windows paths v2

--- healnet_inference.py
+++ healnet_inference.py
@@ -9,7 +9,6 @@ import numpy as np
 import os
 import subprocess
 import urllib.request
-import pathlib as Path
 
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 dir_ =  os.path.join(desktop, "HealNet-Inference")
@@ -24,14 +23,14 @@ except:
 stage_cls = {"Proliferation/Maturation":0,
              "Hemostasis":1,
              "Inflammatory":2}
-root_images = f"{desktop}/Porcine_Exp_Davis" # change this to real folder
+root_images = f"{desktop}/Porcine_Exp_Davis" # potential cause of bugs!
 prob_table_path = f"{desktop}/HealNet-Inference/prob_table.csv"
 
 model = keras.models.load_model(model_path)
 
 # fixing windows path bug as per https://stackoverflow.com/questions/5629242/getting-every-file-in-a-windows-directory
-glob_path = Path(f"{root_images}/**/*.jpg")
-image_paths = glob.glob(glob_path, recursive=True)
+#image_paths = glob.glob(f"{root_images}/**/*.jpg")
+image_paths = os.listdir(root_images)
 
 try:
     prob_table = pd.read_csv(prob_table_path)
@@ -44,24 +43,26 @@ except:
 
 processed_ctr = 0
 for image in tqdm(image_paths):
-    if image not in list(prob_table["Image"]):
-        try:
-            resized_im = Image.open(image).resize((128,128))
-            image_data = img_to_array(resized_im)
-            #image_data = densenet_preprocess(image_data) # densenet hardcoded!
-            image_data = np.expand_dims(image_data, axis=0) # adds batch dim
-            pred = model.predict(image_data, verbose=0)
-            pred = pred.flatten()
-            time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            prob_table.loc[len(prob_table)] = [image, time,
-                                               pred[stage_cls["Hemostasis"]],
-                                               pred[stage_cls["Inflammatory"]],
-                                               pred[stage_cls["Proliferation/Maturation"]]]
-            processed_ctr += 1
-        except:
-            print(f"Unable to open {image} (check if corrupted). Skipping...")
+    if image.endswith(".JPG") or image.endswith(".jpg"):
+        if image not in list(prob_table["Image"]):
+            try:
+                resized_im = Image.open(image).resize((128,128))
+                image_data = img_to_array(resized_im)
+                #image_data = densenet_preprocess(image_data) # densenet hardcoded!
+                image_data = np.expand_dims(image_data, axis=0) # adds batch dim
+                pred = model.predict(image_data, verbose=0)
+                pred = pred.flatten()
+                time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                prob_table.loc[len(prob_table)] = [image, time,
+                                                   pred[stage_cls["Hemostasis"]],
+                                                   pred[stage_cls["Inflammatory"]],
+                                                   pred[stage_cls["Proliferation/Maturation"]]]
+                processed_ctr += 1
+            except:
+                print(f"Unable to open {image} (check if corrupted). Skipping...")
 
 prob_table.to_csv(prob_table_path, index=False)
+
 if processed_ctr:
     print(f"Added {processed_ctr} new predictions to {prob_table_path}")
 else:
