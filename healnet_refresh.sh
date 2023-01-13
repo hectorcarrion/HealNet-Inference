f00465fc4ec9bc580f62dc088705e1f49549e989
hectorcarrion <hcarrion@ucsc.edu>
2023-01-12 20:27:03 -0400

    minor fix

--- healnet_refresh.sh
+++ healnet_refresh.sh
@@ -3,8 +3,9 @@
 USER=$(id -un)
 cd /Users/$USER/Desktop/HealNet-Inference
 git pull
-python3 /Users/$USER/Desktop/HealNet-Inference/docker_run.py
+python3 /Users/$USER/Desktop/HealNet-Inference/healnet_inference.py
 git add prob_table.csv
+git add log.txt
 git commit -m "Table Autocommit"
 git push
 echo Next HealNet run set in 30m
